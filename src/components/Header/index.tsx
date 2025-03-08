f33a19f14c14ff9a3f9ada4fb76b399a3053da50
itseduardolima <eduardolima2417@gmail.com>
2025-03-07 20:35:47 -0400

    feat: add date

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,11 +3,18 @@ import LogoImg from "../../../public/assets/images/Logo.svg";
 import styles from "./Header.module.scss";
 
 export const Header = () => {
+  // Obtém a data atual
+  const today = new Date();
+
+  // Formata a data como "Segunda, 01 de março de 2025"
+  const options: Intl.DateTimeFormatOptions = { weekday: 'long', day: '2-digit', month: 'long', year: 'numeric' };
+  const formattedDate = today.toLocaleDateString('pt-BR', options);
+
   return (
     <header className={styles.header}>
       <Image src={LogoImg} alt="FocalPoint" />
-      <h1>Bem-vindo de volta, Marcus</h1>
-      <span>Segunda, 01 de dezembro de 2025</span>
+      <h1>Bem-vindo de volta, Karine</h1>
+      <span>{formattedDate}</span>
     </header>
   );
 };
